dd23ddf504395bb31f3d9f772ae845ba7b455943
denizderK <denizderya8668@gmail.com>
2023-09-05 18:09:45 +0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <h1>Milestone</h1>
       <h1>Milestone Project</h1>
       <p>New line here</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("tw-elements/dist/plugin")],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
 }
