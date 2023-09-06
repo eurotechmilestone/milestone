8e09c33368e9d83f4c68387e0d3bdf361035814e
Yasin <hacar7703@gmail.com>
2023-09-06 19:33:52 +0200

    join eklendi

---
+++ src/components/Join.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Join = () => {
+  return (
+    <div>
+         <section className="section">
+        <h2>Join Our New Session</h2>
+        <p>Kindedo believes that good questions drive good answers. Whether it's a query <br/> about  the world around us or a challenge.</p>
+        <button className="btn">Apply now</button>
+        <div className="cizgi"></div>
+
+      </section>
+    </div>
+  )
+}
+
+export default Join
