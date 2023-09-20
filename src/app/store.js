a8bce56ed291c19e251a9553c398be0f6a93beea
EuroTechStudy23 <127008228+EuroTechStudy23@users.noreply.github.com>
2023-09-20 07:03:58 +0300

    Revert "Redux fix"

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
-import apiReducer from '../features/ApiSlice'
-import menuReducer from '../features/MenuSlice'
-import languageReducer from '../features/LanguageSlice'
+import homeReducer from '../features/homeSlice'
+import menuReducer from '../features/menuSlice'
+import languageReducer from '../features/languageSlice'
 
 export const store = configureStore({
   reducer: {
-    api:apiReducer,
+    home:homeReducer,
     menu:menuReducer,
-    lang:languageReducer
-  }
+    language:languageReducer,
+  },
 })
--- src/features/languageSlice.js
+++ src/features/languageSlice.js
@@ -1,20 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  preferredLanguage: "en"
+  value: 0,
 }
 
 export const languageSlice = createSlice({
   name: 'language',
   initialState,
   reducers: {
-        setLanguage:(state,{payload}) => {
-            state.preferredLanguage = payload;
-        }
+
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { setLanguage } = languageSlice.actions;
+export const {  } = languageSlice.actions
 
 export default languageSlice.reducer
